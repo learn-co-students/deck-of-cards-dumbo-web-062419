c099c63c92c17b2eb49f50b0437439fba3772fc2
github username <github email address>
2019-07-08 13:57:45 -0400

    Partially done

--- solution.rb
+++ solution.rb
@@ -1,7 +1,20 @@
+# Construct a class for a Deck of cards, which has an attribute cards consisting of an array of 52 Card objects.
 class Deck
+    attr_accessor :cards
 
+    def initialize
+        cards = []
+
+        52.times do
+            cards << Card.new
+        end
+    end
 end
 
 class Card
+    attr_reader :rank, :suit
 
+    def initialize
+        
+    end
 end
